fabf4cad0b2181d1275c538c80690f11506315e6
Prasun <prasun.barthwal@gmail.com>
2019-05-25 14:58:29 +0530

    Added test suite

---
+++ Assignment25/src/test/ProjectTestSuite.java
@@ -0,0 +1,10 @@
+package com.fsd.assignment.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ServiceImplIntegrationTest.class})
+public class ProjectTestSuite {
+    // code relevant to test suite goes here
+}
